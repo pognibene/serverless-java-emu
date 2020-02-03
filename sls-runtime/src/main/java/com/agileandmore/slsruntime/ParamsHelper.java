39d32f1a8813e698486d037c9dec6a1a75fdf119
pascal <pascal.ognibene@artel-solutions.com>
2020-02-03 18:56:40 +0700

    Added Apache license headers

--- sls-runtime/src/main/java/com/agileandmore/slsruntime/ParamsHelper.java
+++ sls-runtime/src/main/java/com/agileandmore/slsruntime/ParamsHelper.java
@@ -1,3 +1,22 @@
+/*
+ * (C) Copyright 2020 Pascal Ognibene (pognibene@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Pascal Ognibene (pognibene@gmail.com)
+ */
+
 package com.agileandmore.slsruntime;
 
 import java.util.Map;
@@ -31,6 +50,7 @@ public class ParamsHelper {
      * @param input input message for the lambda
      * @return the value of the parameter (decoded) or null if not found
      */
+    @SuppressWarnings("unchecked")
     public static String getQueryParameter(String name, Map<String, Object> input) {
         Map<String, String> queryParams = (Map<String, String>) input.get("queryStringParameters");
         if (queryParams != null) {
